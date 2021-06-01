0278d668113af7ff1368e9d28c53d26f4a649897
aravind23457 <yvaravind01@gmail.com>
2021-06-01 19:41:11 +0530

    calculateinterest

---
+++ CalculateIntrest.java
@@ -0,0 +1,7 @@
+package com.aravind.util;
+public class CalculateIntrest{
+public static Double getInstrust(long principal,long t,double rate){
+double instrust=principal* t*rate/100;
+return instrust ;
+}
+}
